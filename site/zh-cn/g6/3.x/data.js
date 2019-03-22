90cd71f06f105217e0fd912fca08c7571e69b7de
Elaine1234 <yilin.qyl@antfin.com>
2019-03-22 11:57:09 +0800

    feat(g6): add demos of g6 3.0

--- site/zh-cn/g6/3.x/data.js
+++ site/zh-cn/g6/3.x/data.js
@@ -2,7 +2,8 @@ const plotByName = {
     tree: { index: 0,  icon: 'icon-tree', url: '#', name: '树图', },
     flow: { index: 1,  icon: 'icon-flow', url: '#', name: '流图', },
     net: { index: 2,  icon: 'icon-net', url: '#', name: '网图', },
-    animate:  { index: 4, icon: 'icon-chord', url: '#', name: '动画' }
+    scatter:  { index: 3, icon: 'icon-scatter', url: '#', name: '动画' },
+    other: { index: 4, icon: 'icon-other', url: '#', name: '其他' }
 };
 module.exports = {
     plotByName,
