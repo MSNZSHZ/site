89f00477da685e64c9c24748f1e458c2e2fa93d1
pddpd <pddpengdi@gmail.com>
2019-07-03 15:07:15 +0800

    fix demo bug (issue 1269), add kefu, and fix about bug

--- site/zh-cn/data.js
+++ site/zh-cn/data.js
@@ -185,6 +185,7 @@ const data = {
         { name: '尽白'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/jinbai.jpg'   , weibo: 'https://www.weibo.com/u/1736341270' , github: 'https://github.com/aa1011372655' } ,
         { name: '绝云'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/jueyun.png'   , weibo: 'https://weibo.com/omosirovincent'   , github: 'https://github.com/leungwensen'  } ,
         { name: '聚则'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/juze.png'     , weibo: ''                                   , github: 'https://github.com/baizn'        } ,
+        { name: '珂甫'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/kefu.png'     , weibo: ''                                   , github: 'https://github.com/pddpd'  } ,
         { name: '陆沉'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/luchen.jpeg'  , weibo: ''                                   , github: ''                                } ,
         { name: '罗宪'  , jobTitle: '动效设计师' , avatar: '${assets}/image/members/luoxian.png'  , weibo: ''                                   , github: ''                                 } ,
         { name: '米法'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/mifa.png'     , weibo: ''                                   , github: 'https://github.com/paleface001'  } ,
